a22f0a00a0268141b4f5c37a1f0c0b53f7da11ec
cnhhoang <nchu0022@student.monash.edu>
2024-02-20 18:52:12 +1100

    Add Negative Array code

---
+++ Negative Array/main.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int Input();
+int Solve();
+
+/* ============================================================================ */
+/* ============================================================================ */
+
+#define fi ""
+#define fo ""
+#define For(i,l,r) for(int i=(int)l; i<=(int)r; i++)
+#define Ford(i,r,l) for(int i=(int)r; i>=(int)l; i--)
+#define pb push_back
+#define ll long long
+#define negativearray(temp,type,name,size) type temp[size]; type * name = temp + (size/2)
+const int infi = 2e9;
+const int mod = 1e9+7;
+const int maxn = 1e5;
+const int N = maxn+10;
+typedef int array[N];
+
+/* ============================================================================ */
+/* ============================================================================ */
+
+int main()
+{
+    Input();
+    Solve();
+}
+
+/* ============================================================================ */
+/* ============================================================================ */
+
+int Input()
+{
+    ios_base::sync_with_stdio(0);
+    freopen(fi, "r", stdin);
+//    freopen(fo, "w", stdout);
+}
+
+/* ============================================================================ */
+
+/* ---------------------------------- */
+
+int Solve()
+{
+
+}
