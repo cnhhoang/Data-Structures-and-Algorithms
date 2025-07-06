1ee57c623f80b409e71dc00c3fcea0a76f6c7457
cnhhoang <cnhhoang.main@gmail.com>
2025-07-06 07:32:51 +0700

    Refactor code

--- BellmanFord/main.cpp
+++ BellmanFord/main.cpp
@@ -1,88 +1,109 @@
 #include <cstdio>
+
 int Input();
 int BellmanFord();
 int Output();
 
 /* ================================================================================= */
 
-const int maxn = 1000;
-const int maxm = maxn*(maxn-1)/2;
-const int infi = 1000000000;
-typedef int mang[maxn+10];
-typedef int dscanh[maxm+10];
-dscanh u, v, c;    int n, m;
-int s, t;
-mang d, trace;
+const int MAXN = 1000;
+const int MAXM = MAXN * (MAXN - 1) / 2;
+const int INF = 1000000000;
+
+typedef int IntArray[MAXN + 10];
+typedef int EdgeArray[MAXM + 10];
+
+EdgeArray from, to, cost;
+int n, m;
+int source, target;
+IntArray distance, predecessor;
 
 /* ================================================================================= */
 
-int main()
+int main() 
 {
     Input();
     BellmanFord();
     Output();
+
+    return 0;
 }
 
 /* ================================================================================= */
 /* ================================================================================= */
 
-int Input()
+int Input() 
 {
     freopen("BELLMANFORD.INP", "r", stdin);
-    scanf("%i %i %i %i", &n, &m, &s, &t);
-    for (int i=1; i<=m; i++)
-    scanf("%i %i %i", &u[i], &v[i], &c[i]);
-}
 
-/* ------------------------------------ */
+    scanf("%d %d %d %d", &n, &m, &source, &target);
 
-int Output()
-{
-    printf("%i\n", d[t]);
-    mang st;    int top=0;
-    while (t!=s)    {
-        st[++top] = t;
-        t = trace[t];
-    }
-    st[++top] = s;
-    while (top) {
-        printf("%i ", st[top]);
-        top--;
-    }
-}
+    for (int i = 1; i <= m; ++i)
+        scanf("%d %d %d", &from[i], &to[i], &cost[i]);
 
-/* ================================================================================= */
-
-int Init()
-{
-    for (int i=1; i<=n; i++)
-        d[i] = infi,   trace[i] = 0;
-    d[s] = 0;
-}
-
-/* ------------------------------------ */
-
-int Relax(int u, int v, int c)
-{
-    if (d[u]!=infi && d[v]>d[u]+c)  {
-        d[v] = d[u]+c;
-        trace[v] = u;
-        return 1;
-    }
     return 0;
 }
 
 /* ------------------------------------ */
 
-int BellmanFord()
+int Output() 
 {
-    Init();
-    while (1)
-    {
-        bool stop = true;
-        for (int i=1; i<=m; i++)
-            if (Relax(u[i], v[i], c[i]))
-                stop = false;
-        if (stop)   break;
+    printf("%d\n", distance[target]);
+
+    IntArray stack;
+    int top = 0;
+
+    for (int current = target; current != source; current = predecessor[current])
+        stack[++top] = current;
+    stack[++top] = source;
+
+    while (top)
+        printf("%d ", stack[top--]);
+
+    return 0;
+}
+
+/* ================================================================================= */
+
+int Init() 
+{
+    for (int i = 1; i <= n; ++i) {
+        distance[i] = INF;
+        predecessor[i] = 0;
     }
+
+    distance[source] = 0;
+
+    return 0;
+}
+
+/* ------------------------------------ */
+
+int Relax(int u, int v, int w) {
+    if (distance[u] != INF && distance[v] > distance[u] + w) {
+        distance[v] = distance[u] + w;
+        predecessor[v] = u;
+
+        return 1;
+    }
+
+    return 0;
+}
+
+/* ------------------------------------ */
+
+int BellmanFord() {
+    Init();
+    
+    while (true) {
+        bool updated = false;
+
+        for (int i = 1; i <= m; ++i)
+            if (Relax(from[i], to[i], cost[i]))
+                updated = true;
+
+        if (!updated) break;
+    }
+    
+    return 0;
 }
