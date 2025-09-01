c3c60cdcf27b223a63bebe7ce93a4cb122077959
cnhhoang <cnhhoang.main@gmail.com>
2025-09-02 01:52:01 +0700

    Refactor: convert Interval Tree into reusable class (lazy propagation version)

--- IT/Lazy Propagation/main.cpp
+++ IT/Lazy Propagation/main.cpp
@@ -6,120 +6,143 @@
 
 #include <cstdio>
 #include <iostream>
+#include <vector>
 using namespace std;
-int Input();
-int Solve();
 
-/* ================================================================================= */
-/* ================================================================================= */
-
-const int maxn = 100000;
 typedef long long ll;
-typedef ll array[maxn*4+10];
 
-array a; int n, q;
-array s, lazy, l, r;
+class LazyIntervalTree {
+private:
+    int num_elements;
+    std::vector<ll> segmentValue, lazy, leftBound, rightBound;
 
-/* ================================================================================= */
-/* ================================================================================= */
+    // Helper functions
+    void recalculate(int node) {
+        if (leftBound[node] == rightBound[node])
+            return;
+        int leftChild = node * 2 + 1;
+        int rightChild = node * 2 + 2;
+        segmentValue[node] = segmentValue[leftChild] + segmentValue[rightChild];
+    }
 
-int main()
-{
-    Input();
-    Solve();
-}
+    // Main functions
+    void build(int node, int left, int right, const std::vector<ll>& arr) {
+        leftBound[node] = left;
+        rightBound[node] = right;
+        
+        if (leftBound[node] == rightBound[node]) {
+            segmentValue[node] = arr[left];
+        } else {
+            int mid = (left + right) / 2;
+            build(node * 2 + 1, left, mid, arr);
+            build(node * 2 + 2, mid + 1, right, arr);
+            recalculate(node);
+        }
+    }
 
-/* ================================================================================= */
-/* ================================================================================= */
+    void diffuse(int node) {
+        if (!lazy[node])
+            return;
+            
+        segmentValue[node] += lazy[node] * (rightBound[node] - leftBound[node] + 1); 
 
-int Input()
-{
+        if (leftBound[node] != rightBound[node]) { // Not a leaf node
+            lazy[node * 2 + 1] += lazy[node];
+            lazy[node * 2 + 2] += lazy[node];
+        }
+
+        lazy[node] = 0;
+    }
+
+    void update(int node, int left, int right, ll value) {
+        diffuse(node); // Diffuse the lazy value to the children
+
+        if (rightBound[node] < left || right < leftBound[node]) // Not in the range
+            return;
+
+        if (left <= leftBound[node] && rightBound[node] <= right) { // Fully in the range
+            lazy[node] += value;
+            diffuse(node);
+        } else { // Partially in the range
+            update(node * 2 + 1 , left, right, value);
+            update(node * 2 + 2, left, right, value);
+            recalculate(node);
+        }
+    }
+
+    ll query(int node, int left, int right) {
+        diffuse(node);
+        
+        if (rightBound[node] < left || right < leftBound[node]) // Not in the range
+            return 0;
+        if (left <= leftBound[node] && rightBound[node] <= right) // Fully in the range
+            return segmentValue[node];
+        return query(node * 2 + 1, left, right) + query(node * 2 + 2, left, right);
+    }
+
+public:
+    LazyIntervalTree(const std::vector<ll>& arr) 
+        : num_elements(arr.size()) 
+    {
+        int sz = num_elements * 4 + 10;
+        segmentValue.assign(sz, 0);
+        lazy.assign(sz, 0);
+        leftBound.assign(sz, -1);
+        rightBound.assign(sz, -1);
+        build(0, 0, num_elements - 1, arr);
+    }
+
+    void Update(int left, int right, ll value) {
+        update(0, left, right, value);
+    }
+
+    ll Query(int left, int right) {
+        return query(0, left, right);
+    }
+
+    void PrintTree() {
+        printf("Segment Tree Values:\n");
+        for (int node = 0; node < num_elements * 4 + 10; node++) {
+            if (leftBound[node] != -1 && rightBound[node] != -1)
+                printf("Node %i: [%i, %i] = %lld (lazy: %lld)\n", 
+                       node, leftBound[node], rightBound[node], 
+                       segmentValue[node], lazy[node]);
+        }
+    }
+};
+
+//====================================================================================================
+
+int main() {
+    // Input
+    const bool DEBUG = false;
+    int n, q;
+    std::vector<ll> a;
+    
     freopen("LAZYPROPAGATION.INP", "r", stdin);
     scanf("%i %i", &n, &q);
-    for (int i=1; i<=n; i++)
-        scanf("%i", &a[i]);
-}
+    a.resize(n); // 1-indexed
+    for (int i = 0; i < n; i++)
+        scanf("%lld", &a[i]);
 
-/* ================================================================================= */
-
-int BuildTree(int x, int left, int right)
-{
-    l[x] = left;    r[x] = right;
-    if (l[x]==r[x])
-        s[x] = a[left];
-    else    {
-        int m = (left+right)/2;
-        BuildTree(x*2, left, m);
-        BuildTree(x*2+1, m+1, right);
-        s[x] = s[x*2] + s[x*2+1];
-    }
-}
-
-/* ------------------------------------- */
-
-int Diffuse(int x)
-{
-    if (!lazy[x])
-        return 0;
-    s[x] += lazy[x] * (r[x]-l[x]+1); // lazy nhan cho so phan tu x quan ly
-
-    if (l[x]!=r[x]) { // khong phai nut la
-        lazy[x*2] += lazy[x];
-        lazy[x*2+1] += lazy[x];
-    }
-    lazy[x] = 0;
-}
-
-/* ------------------------------------- */
-
-int Update(int x, int left, int right, int v)
-{
-    Diffuse(x);
-    if (r[x] < left || right < l[x]) // klq
-        return 0;
-    if (left <= l[x] && r[x] <= right)    { // thuoc hoan toan
-        s[x] += v * (r[x]-l[x]+1);
-        if (l[x]==r[x])
-            return 0;
-        lazy[x*2] += v;
-        lazy[x*2+1] += v;
-    }
-    else    {
-        Update(x*2, left, right, v);
-        Update(x*2+1, left, right, v);
-        s[x] = s[x*2] + s[x*2+1];
-    }
-}
-
-/* ------------------------------------- */
-
-int Query(int x, int left, int right)
-{
-    Diffuse(x);
-    if (r[x] < left || right < l[x]) // klq
-        return 0;
-    if (left <= l[x] && r[x] <= right)
-        return s[x];
-    return Query(x*2, left, right) + Query(x*2+1, left, right);
-}
-
-/* ------------------------------------- */
-
-int Solve()
-{
-    BuildTree(1, 1, n);
+    // Process
+    LazyIntervalTree lazyTree(a);
+    if (DEBUG) lazyTree.PrintTree();
 
     // Truy van
-    int choice, x, y, v;
-    for (int k=1; k<=q; k++)    {
+    int choice, x, y;
+    ll v;
+    for (int k = 1; k <= q; k++) {
         scanf("%i", &choice);
-        if (choice==0)  {
-            scanf("%i %i %i", &x, &y, &v);
-            Update (1, x, y, v);
-        }
-        else    {
+        if (choice == 0) {
+            scanf("%i %i %lld", &x, &y, &v);
+            if (DEBUG) printf("Update [%i, %i] += %lld\n", x, y, v);
+            lazyTree.Update(x-1, y-1, v);
+            if (DEBUG) lazyTree.PrintTree();
+        } else {
             scanf("%i %i", &x, &y);
-            cout << Query(1, x, y) << endl;
+            if (DEBUG) printf("Query [%i, %i] = ", x, y);
+            cout << lazyTree.Query(x-1, y-1) << endl;
         }
     }
 }
